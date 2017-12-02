64332e5c38934868f76aecf2f45f074275dc172f
Hilmi Raditya <raditya113@gmail.com>
2017-12-02 17:36:28 +0700

    Hilmi
    Tambahan  Main Menu

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
+#include <iostream>
+#include "Menu.h"
+
 using namespace sf;
 
 const int W=600;
@@ -32,9 +35,9 @@ struct player
   {return Vector3f(color.r,color.g,color.b);}
 };
 
-int main()
+void a()
 {
-	srand(time(0));
+    	srand(time(0));
 
     RenderWindow window(VideoMode(W, H), "The Tron!");
     window.setFramerateLimit(60);
@@ -127,5 +130,65 @@ int main()
  		window.display();
 	}
 
+}
+
+int main()
+{
+    srand(time(0));
+	sf::RenderWindow window(sf::VideoMode(600, 600), "TRON");
+	Menu menu(window.getSize().x, window.getSize().y);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::KeyReleased:
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Up:
+					menu.MoveUp();
+					break;
+
+				case sf::Keyboard::Down:
+					menu.MoveDown();
+					break;
+
+				case sf::Keyboard::Return:
+					switch (menu.GetPressedItem())
+					{
+					case 0:
+						std::cout << "Play button has been pressed" << std::endl;
+						window.clear();
+						window.close();
+						a();
+						break;
+					case 1:
+						std::cout << "Option button has been pressed" << std::endl;
+						break;
+					case 2:
+						window.close();
+						break;
+					}
+
+					break;
+				}
+
+				break;
+			case sf::Event::Closed:
+				window.close();
+
+				break;
+
+			}
+		}
+
+		window.clear();
+		menu.draw(window);
+		window.display();
+	}
     return 0;
 }
