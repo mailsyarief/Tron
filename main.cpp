7312b1b6acf9a6fb8eafc69e5b9e2103708e39c8
MAILE\Ismail <ismail8syarief@gmail.com>
2017-12-03 07:58:12 +0700

    add comment

--- main.cpp
+++ main.cpp
@@ -23,30 +23,6 @@ void MainMenu();
 void OptionsFunction();
 void AboutFunction();
 
-/*struct player
-{ int x,y,dir;
-  Color color;
-  player(Color c)
-  {
-    x=rand() % W;
-    y=rand() % H;
-    color=c;
-    dir=rand() % 4;
-  }
-  void tick()
-  {
-    if (dir==0) y+=1;
-    if (dir==1) x-=1;
-    if (dir==2) x+=1;
-    if (dir==3) y-=1;
-
-	if (x>=W) x=0;  if (x<0) x=W-1;
-    if (y>=H) y=0;  if (y<0) y=H-1;
-  }
-
-  Vector3f getColor()
-  {return Vector3f(color.r,color.g,color.b);}
-};*/
 
 void a()
 {
